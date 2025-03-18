8ecfcda1d7aba4740c0bb9e79bb01375d638747f
abhishek <abhisec.tech@gmail.com>
2025-03-18 23:55:22 +0530

    sitemap url changed

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,4 @@
-const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://gamora.world";
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
